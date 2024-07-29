62f84ed786a5ea2945b3efdf53b39a3aaab676e5
Alfred  Fokoung Douo <alfokoung@gmail.com>
2024-07-29 10:26:01 +0200

    Responsive of headers

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:my_portfolio/constants/colors.dart';
+import 'package:my_portfolio/constants/size.dart';
 import 'package:my_portfolio/widgets/header_mobile.dart';
 
 import '../widgets/drawer_mobile.dart';
+import '../widgets/header_desktop.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,41 +18,52 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final scaffoldKey = GlobalKey<ScaffoldState>();
-    return Scaffold(
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
         key: scaffoldKey,
-        endDrawer: DrawerMobile(),
+        endDrawer: constraints.maxWidth >= kMinDesktopWidth
+            ? null
+            : const DrawerMobile(),
         backgroundColor: CustomColor.scaffoldBg,
         // use ListView for scrollable list
-        body: ListView(children: [
-          // MAIN
-          //HeaderDesktop(),
-          HeaderMobile(
-            onLogoTap: () {},
-            onMenuTap: () {
-              scaffoldKey.currentState?.openEndDrawer();
-            },
-          ),
-          // SKILLS
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            // MAIN
 
-          // PROJECTS
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            //color: Colors.blueGrey,
-          ),
+            if (constraints.maxWidth >= kMinDesktopWidth)
+              const HeaderDesktop()
+            else
+              HeaderMobile(
+                onLogoTap: () {},
+                onMenuTap: () {
+                  scaffoldKey.currentState?.openEndDrawer();
+                },
+              ),
+            // SKILLS
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
 
-          // CONTACT
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
-          // FOOTER
-        ]));
+            // PROJECTS
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              //color: Colors.blueGrey,
+            ),
+
+            // CONTACT
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
+            // FOOTER
+          ],
+        ),
+      );
+    });
   }
 }
